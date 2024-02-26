216e3c9bc8a8b2af12d318da5f0cc4084f643002
Diego Faravelli <diego.faravelli@oncode.it>
2024-02-26 20:55:26 +0100

    Revert "initial commit"
    This reverts commit b0d1e867c20364114621e49e4fbec4eeb2b62685.

--- src/main/java/it/oncode/mongo/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package it.oncode.mongo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import it.oncode.mongo.model.Address;
-
-public interface AddressRepository extends MongoRepository<Address, String>{
-    
-}
